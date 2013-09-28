a979f0fe0336c1db71e29dd17182e48b46663294
Jacky <jingcmu@qq.com>
2013-09-28 13:36:19 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PETBugShooters::Application.config.secret_token = '6019e72522bb747bda72b59eba6f1d397dec28d7e380cb7954cb9495684eb07c9552335f680f04b357ddb43130ad1068b752675a37a9e5678130760d4eb60ef8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PETBugShooters::Application.config.session_store :cookie_store, key: '_PET_bug_shooters_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PETBugShooters::Application.config.session_store :active_record_store
